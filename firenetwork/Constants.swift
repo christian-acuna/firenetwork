1bf2d0f742b6fcbe6a639553391ec16c725251fb
Christian Acuna <cacuna0828@gmail.com>
2016-04-06 16:30:32 +0800

    Sign In / Sign Up Storyboard Set with custom material design UI

---
+++ firenetwork/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  firenetwork
+//
+//  Created by Christian on 4/6/16.
+//  Copyright © 2016 Crossroads. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//everything we put in here is globally accessible
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
